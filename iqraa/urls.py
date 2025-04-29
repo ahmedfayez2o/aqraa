2a6c88fc9e882f91878aa66e7ee842be2097b5e7
AhmedFayez <ahmedfayezd620@gmail.com>
2025-04-29 07:48:07 +0300

    edit the endpoints

--- iqraa/urls.py
+++ iqraa/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,5 @@ urlpatterns = [
     path('api/', include('orders.urls')),
     path('api/', include('reviews.urls')),
     path('api/', include('recommendations.urls')),
+    path('api/docs/', include_docs_urls(title='Iqraa API Documentation')),
 ]
